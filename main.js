b50ffaabe0388e7fcc9feddf9b2033979df5cfaa
Kstyle0710 <jongbaekim0710@gmail.com>
2020-07-07 21:56:17 +0900

    my 1st javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+// html  open with live server 구동후 ctrl+shift_i 누르면 콘솔창 보임
+'use strict';
+
+console.log('hello world');
+console.log('hello world2');
+
+let a;
+a = 6;
+console.log(a);
+
+function sayThis(something) {
+  console.log(something);
+}
+
+sayThis('1234');
