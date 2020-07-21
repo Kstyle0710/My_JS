c632c3116c67ab94b3d49f47847bfb7461977ef6
Kstyle0710 <jongbaekim0710@gmail.com>
2020-07-21 17:24:17 +0900

    todo list 편집 수정

--- greeting.js
+++ greeting.js
@@ -24,7 +24,7 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `Hello ${text}`;
+    greeting.innerHTML = `Good Day! ${text}`;
 }
 
 function loadName() {
